f22c5ca9d6a195638e828e90c5cd358931bece98
vitorluzsantos <vitor.lsantos@sptech.school>
2026-03-09 12:18:30 -0300

    feat:index.html, script.js, style.css

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function calcular(){
+	let kilos = Number(idLote.value);
+	let preco = Number(idPreco.value);
+	let graus = Number(idGraus.value);
+
+	let perda = preco * kilos;
+	if(graus <= 4){
+		idResultado.innerHTML = `
+🌡️ Temperatura atual: ${graus} C° <br>
+✅ Ideal para o refrigeramento <br>
+Possível perda caso saia do padrão: R$ <p> ${perda.toFixed(2)} </p>`;
+	} else {
+		idResultado.innerHTML = `
+🌡️ Temperatura atual: ${graus} C° <br>
+⚠️ <p>Não está apropriada para o refrigeramento</p> <br>
+Possível perda caso saia do padrão: R$<p>${perda.toFixed(2)} </p>`;
+	}
+}
